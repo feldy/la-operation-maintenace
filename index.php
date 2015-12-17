bf02992514d4ab7e93d53696f2df9e4cd2f565ef
Feldy Yusuf <f3ldy10rpl@gmail.com>
2015-12-17 21:31:56 +0700

    first push

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+	<head>
+
+	    <meta charset="utf-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	    <title>Dashboard</title>
+
+	    <link href="lib/css/bootstrap.min.css" rel="stylesheet">
+	    <link href="lib/font-awesome/css/font-awesome.css" rel="stylesheet">
+
+	    <link href="lib/css/animate.css" rel="stylesheet">
+	    <link href="lib/css/style.css" rel="stylesheet">
+
+	</head>
+	<body class="">
+	    <div id="wrapper" >
+		    <!-- <div id="page-wrapper" class="gray-bg"> -->
+		    	<div class="wrapper wrapper-content text-center animated fadeInRight gray-bg">
+		    		<div class="row">
+		    			<nav class="navbar navbar-static-top" role="navigation" style="margin-bottom: 0">
+				            <ul class="nav navbar-top-links navbar-right">
+				                <li class="dropdown">
+				                    <a class="dropdown-toggle count-info" data-toggle="dropdown" href="#">
+				                        Notification <i class="fa fa-bell"></i>  <span class="label label-primary">8</span>
+				                    </a>
+				                    <ul class="dropdown-menu dropdown-alerts">
+				                        <li>
+				                            <a href="#">
+				                                <div>
+				                                    <i class="fa fa-envelope fa-fw"></i> You have 16 messages
+				                                    <span class="pull-right text-muted small">4 minutes ago</span>
+				                                </div>
+				                            </a>
+				                        </li>
+				                        <li class="divider"></li>
+				                        <li>
+				                            <a href="#">
+				                                <div>
+				                                    <i class="fa fa-twitter fa-fw"></i> 3 New Followers
+				                                    <span class="pull-right text-muted small">12 minutes ago</span>
+				                                </div>
+				                            </a>
+				                        </li>
+				                        <li class="divider"></li>
+				                        <li>
+				                            <a href="#">
+				                                <div>
+				                                    <i class="fa fa-upload fa-fw"></i> Server Rebooted
+				                                    <span class="pull-right text-muted small">4 minutes ago</span>
+				                                </div>
+				                            </a>
+				                        </li>
+				                        <li class="divider"></li>
+				                        <li>
+				                            <div class="text-center link-block">
+				                                <a href="notifications.html">
+				                                    <strong>See All Alerts</strong>
+				                                    <i class="fa fa-angle-right"></i>
+				                                </a>
+				                            </div>
+				                        </li>
+				                    </ul>
+				                </li>
+				            </ul>
+				        </nav>
+		    		</div>
+		    		<div class="row">
+			        	<div class="col-lg-3">
+		                    <img src="images/logo.png" class="img-responsive">
+		                    <!-- <p class="small">Maintenance</p> -->
+			        		<h1>Maintenance</h1>
+		    			</div>
+		    		</div>
+		    		<br /><br /><br />
+		    		<div class="row">
+			        	<div class="col-lg-3">
+			            	<button class="btn btn-primary dim btn-large-dim" style="width: 280px;" type="button"><i class="fa fa- fa-paper-plane-o"></i> VSAT</button>
+	                        <button class="btn btn-warning dim btn-large-dim" style="width: 280px;" type="button"><i class="fa fa-wifi"></i> Wireless</button>
+	                        <button class="btn btn-danger  dim btn-large-dim" style="width: 280px;" type="button"><i class="fa fa-plug"></i> Wireline</button>
+	                        <button class="btn btn-danger  dim btn-large-dim" style="width: 280px;" type="button"><i class="fa fa- fa-pied-piper-alt"></i> About</button>
+		            	</div>
+		            	<div class="col-lg-3">
+		            		<!-- <button class="btn btn-primary  dim btn-large-dim" type="button"><i class="fa fa-dollar"></i>6</button> -->
+		            	</div>
+		    		</div>
+				</div>
+			<!-- </div> -->
+	    </div>
+	</body>
+	 <!-- Mainly scripts -->
+    <script src="lib/js/jquery-2.1.1.js"></script>
+    <script src="lib/js/bootstrap.min.js"></script>
+    <script src="lib/js/plugins/metisMenu/jquery.metisMenu.js"></script>
+    <script src="lib/js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+
+    <!-- Custom and plugin javascript -->
+    <script src="lib/js/inspinia.js"></script>
+    <script src="lib/js/plugins/pace/pace.min.js"></script>
+</html>
