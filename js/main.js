cb11abf543cbe2adf3d83d7587fc7c8e69eecf0c
Feldy Yusuf <f3ldy10rpl@gmail.com>
2016-02-22 23:10:42 +0700

    modify JS

--- js/main.js
+++ js/main.js
@@ -1,8 +1,4 @@
 $(document).ready(function(){
-	for (var i = 1; i <= 13; i++) {
-		new Switchery(document.querySelector('.js-switch'+i), { color: '#1AB394' });
-	}
-
 	$('.summernote').summernote({
 		toolbar: [				 
 			['style', ['bold', 'italic', 'underline', 'clear']],
