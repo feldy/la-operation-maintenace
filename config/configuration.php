8a2e0a7a25ba9582d85e3ec577ab5731dfcd0177
Feldy Yusuf <f3ldy10rpl@gmail.com>
2016-02-25 21:56:17 +0700

    update

--- config/configuration.php
+++ config/configuration.php
@@ -2,8 +2,10 @@
 	// mysql_connect("localhost","root","admin");
 	// mysql_select_db('db_lintas_arta') or die(mysql_error());
 
-	mysql_connect("mysql.idhostinger.com","u346528017_root","admin");
-	mysql_select_db('u346528017_la') or die(mysql_error());
+	// mysql_connect("mysql.idhostinger.com","u346528017_root","admin");
+	// mysql_select_db('u346528017_la') or die(mysql_error());
+
+	$con = mysqli_connect("mysql.idhostinger.com","u346528017_root","admin","u346528017_la");
 
 
 	function gen_uuid() {
