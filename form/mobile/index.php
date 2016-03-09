740c5aab494afcd0231ffda1d31b1f659a30c462
Feldy Yusuf <f3ldy10rpl@gmail.com>
2016-03-09 22:31:19 +0700

    isi laporan

--- form/mobile/index.php
+++ form/mobile/index.php
@@ -91,6 +91,111 @@
             </div>
             <?php } ?>
         <?php } else if (isset($_GET['page']) && $_GET['page'] == "laporan_selesai") { ?>
+        <?php 
+        $value_str_search = "";
+        $strSearch = "";
+        ?>
+        <div class="col-sm-12">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="ibox float-e-margins">
+                        <div class="ibox-title">
+                            <h5>Daftar Maintenance Pelanggan</h5>
+                            <div class="ibox-tools">
+                                <a class="collapse-link">
+                                    <i class="fa fa-chevron-up"></i>
+                                </a>
+                                <a class="close-link">
+                                    <i class="fa fa-times"></i>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="ibox-content">
+                            <div class="row">
+                                <div class="col-sm-8">
+                                </div>
+                                <div class="col-sm-4">
+                                    <form method="post" action="">
+                                        <div class="input-group">
+                                            <input type="text" name="src_search" value="<?php echo $value_str_search;?>" placeholder="Search by No SPK, Team, Customer dan Akses" class="input-sm form-control" /> 
+                                            <span class="input-group-btn">
+                                                <button type="submit" class="btn btn-sm btn-primary"> Go!</button> 
+                                            </span>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                            <table class="table table-hover no-margins">
+                                <!-- <thead>
+                                <tr>
+                                    <th width="160px">Tanggal </th>
+                                    <th>No. SPK</th>
+                                    <th>Pelanggan</th>
+                                    <th>Alamat</th>
+                                    <th>Akses</th>
+                                    <th>Keluhan</th>
+                                    <th>Team</th>
+                                    <th></th>
+                                </tr>
+                                </thead> -->
+                                <tbody>
+                                <?php 
+                                    $str = "SELECT  DATE_FORMAT(spk.tanggal, '%d/%m/%Y %h:%i:%s') as tanggal, spk.sid as sid_spk,
+                                                    no_spk, status, akses, masalah, pel.nama_pelanggan as nama_pelanggan, pel.alamat as alamat, tim.nama_team as nama_team, tim.leader as leader
+                                            FROM t_surat_perintah_kerja spk 
+                                            LEFT JOIN m_pelanggan pel ON spk.id_pelanggan = pel.sid 
+                                            LEFT JOIN m_team_header tim ON spk.id_team = tim.sid 
+                                            $strSearch 
+                                            ORDER BY spk.tanggal desc
+                                            ";
+
+                                    // echo $str;
+                                    $result = mysqli_query($conn, $str) or die(mysqli_error());
+
+                                    while($arr=mysqli_fetch_array($result)) {
+                                ?>
+                                <tr>
+                                    <td class="project-status">
+                                        <span class="label label-primary"><?php echo $arr['akses'] ?></span>
+                                        <br/>
+                                        <small><?php echo $arr['no_spk'] ?></small>
+                                    </td>
+                                    <td class="project-title">
+                                        <a href="#"><?php echo $arr['nama_pelanggan'] ?></a>
+                                        <br/>
+                                        <small><i class="fa fa-clock-o"></i> <?php echo $arr['alamat'] ?></small>
+                                    </td>
+                                    <td class="project-title">
+                                        <a>Keluhan: <?php echo $arr['masalah'] ?></a>
+                                        <br/>
+                                        <small><i class="fa fa-clock-o"></i> <?php echo $arr['tanggal'] ?></small>
+                                    </td>
+                                    <td class="project-status">
+                                        <span class="label label-info">Team: <?php echo $arr['nama_team'] ?></span>
+                                        <br/>
+                                        <small><i class="fa fa-user"></i> <?php echo $arr['leader'] ?></small>
+                                    </td>
+                                    <form method="post" action="" id="form_cancel">
+                                    <td class="project-actions">
+                                        <?php if ($arr['status'] == "INPROGRESS") { ?>   
+                                        <a href="?form=view_detail&akses=<?php echo $arr['akses'];?>&sid=<?php echo $arr['sid_spk'];?>" class="btn btn-white btn-sm"><i class="fa fa-folder"></i> View </a>
+                                        <?php } else if ($arr['status'] == "NEW" || $arr['status'] == "INPROGRESS") { ?>
+                                        <button type="submit" name="action_cancel" value="<?php echo $arr['sid_spk'];?>" class="btn btn-white btn-sm"><i class="fa fa-ban"></i> Cancel </button>
+                                        <!-- <a class="btn btn-warning btn-sm"><i class="fa fa-yelp"></i> Inprogress.. </a> -->
+                                        <?php } else { ?>
+                                        <a class="btn btn-warning btn-sm"><i class="fa fa-yelp"></i> <?php echo $arr['status'];?> </a>
+                                        <?php } ?>
+                                    </td>
+                                    </form>
+                                </tr>
+                                <?php }  ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <?php } else { ?>
         <div class="col-lg-12">
             <div class="middle-box text-center loginscreen  animated fadeInDown" style="margin-top: 0px;">
@@ -112,7 +217,7 @@
                             </tr>
                         </table>
                     </button>
-                    <button class="btn btn-danger  dim btn-large-dim" style="width: 100%;" type="button">
+                    <button class="btn btn-danger  dim btn-large-dim" style="width: 100%;" type="button" onclick="window.location.href = 'home.php'">
                         <table width="100%">
                             <tr>
                                 <td width="40px"><i class="fa fa-edit"></i></td>
